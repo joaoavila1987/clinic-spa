86ccf12703c8fd2106fffb37674468ec8b6cc9cb
Joao Avila <joaoavila.py@gmail.com>
2022-07-01 21:06:57 -0300

    troca de logo no login e analise de estrutura

--- src/componentes/main/App.js
+++ src/componentes/main/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+//import DashBoard from '../templates/corpo/Dashboard';
+import Login from '../login/SignIn';
 
 function App() {
   return (
     <div>
-    
+      {/* <DashBoard /> */}
+      <Login />
     </div>
   );
 }
