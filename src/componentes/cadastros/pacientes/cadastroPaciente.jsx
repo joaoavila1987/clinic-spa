f9a1be68f4533da3797b1e1593994d4ba29930ac
Joao Avila <joaoavila.py@gmail.com>
2022-07-11 21:14:30 -0300

    trabalhando na tela de cadastro de paciente

--- src/componentes/cadastros/pacientes/cadastroPaciente.jsx
+++ src/componentes/cadastros/pacientes/cadastroPaciente.jsx
@@ -1,24 +1,21 @@
 import React, { Component } from 'react'
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 
 export default class cadastroPaciente extends Component {
   render() {
     return (
       <>
-      <Container >
-            <Grid container>
-                <Grid>
-                    <Box sx={{ bgcolor:"red"}}>
-                        <Typography>
-                            Cadastro de Paciente 
-                        </Typography>
-                    </Box>
-                </Grid>
-            </Grid>
-      </Container>  
+        <Grid container> 
+          <Grid item  sx={{alignItems:"center"}} xs={12} md={12} lx={12}>
+              <Box>                          
+                  <Typography>
+                      Cadastro de Paciente 
+                  </Typography>
+              </Box>
+          </Grid>
+        </Grid>  
       </>
     )
   }
